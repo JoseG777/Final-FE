f144d738cd53437833efe5ed8819365845c36121
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-05-17 22:05:08 -0400

    updated home page

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -4,11 +4,17 @@ HomePageView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
+import { Link } from "react-router-dom";
 const HomePageView = () => {
   // Render Home page view
   return (
     <div >
-      <h1>Home Page</h1>
+      <h1>Welcome to our Full-Stack CRUD Application!</h1>
+      <h1> The purpose of this application is to allow users to efficiently manage their campuses </h1>
+      <h1> As a user, you will be able to view campuses and students, add campuses and students, and edit existing campuses and students! </h1>
+
+      <h2>Click the top right or here <Link to="/campuses">here</Link> to see all campuses</h2>
+      <h2>Click the top right or here <Link to="/students">here</Link> to see all students</h2>
     </div>
   );    
 }
